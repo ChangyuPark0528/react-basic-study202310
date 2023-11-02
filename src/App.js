0deb366c0443005369ad5a8b49083f43544b2dd0
changyu <changyu1061@naver.com>
2023-11-02 16:11:56 +0900

    props 개념 / 컴포넌트분할 / useState / 동적리스트렌더링 / Chart구현

--- src/App.js
+++ src/App.js
@@ -1,34 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Expenseltem from './components/Expenseltem';
-import NoName from './noname';
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
-function App() {
-  const $h1 = <h1>메롱메로옹</h1>;
-  const $h2 = <h2>반가워용~~~~~~~~~~</h2>;
+// import Hello from './Hello';
+
+const App = () => {
+  // 지출 항목 객체 배열
+  const expenses = [
+    {
+      id: 1,
+      title: '떡볶이+순대',
+      price: 18000,
+      date: new Date(2023, 3 - 1, 23),
+    },
+    {
+      id: 2,
+      title: '청년다방 페퍼로니 떡볶이',
+      price: 21000,
+      date: new Date(2023, 5 - 1, 21),
+    },
+    {
+      id: 3,
+      title: '반올림피자 페퍼로니',
+      price: 25000,
+      date: new Date(2020, 7 - 1, 4),
+    },
+    {
+      id: 4,
+      title: '엽기떡볶이 마라맛',
+      price: 23000,
+      date: new Date(2023, 11 - 1, 1),
+    },
+    {
+      id: 5,
+      title: '타코야끼',
+      price: 4500,
+      date: new Date(2021, 12 - 1, 23),
+    },
+  ];
+
+  // 지출 객체 배열을 상태 변수로 관리
+  const [expenseList, setExpenseList] = useState(expenses);
+
+  // ExpenseForm에게 내려보낼 함수
+  const addExpenseHandler = (newExpense) => {
+    console.log('App 컴포넌트에서 응답함!');
+
+    const modifyExpense = {
+      ...newExpense,
+      id: expenseList[expenseList.length - 1].id + 1,
+    };
+    console.log(modifyExpense);
+
+    setExpenseList([...expenseList, modifyExpense]);
+    console.log(expenseList);
+  };
 
   return (
     <>
-      <NoName />
-      <Expenseltem />
-      <Expenseltem />
-      <Expenseltem />
-      <Expenseltem />
-      <Expenseltem />
-      <div className='App'>
-        {$h1}
-        <h1>메롱메로옹</h1>
-        {$h2}
-      </div>
-      <div className='noname'>
-        <input type='text' />
-        <p>
-          오늘은 월요일입니다~~~ <br />
-          그래서 공부하기 싫어요..사실맨날임.
-        </p>
-      </div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenseList} />
     </>
   );
-}
+};
 
 export default App;
